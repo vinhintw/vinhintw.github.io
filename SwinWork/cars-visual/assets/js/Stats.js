efd25ecb51aa54a6f8667828831739683550d9c3
vinh <vinhintw2014@gmail.com>
2023-11-02 23:29:28 +0800

    update

--- SwinWork/cars-visual/assets/js/Stats.js
+++
@@ -1,8 +0,0 @@
-window.stats = function (data) {
-    var stats = {};
-    
-    document.getElementById("n_cars").innerHTML  = data.length;
-    document.getElementById("n_vars").innerHTML  = Object.keys(data[0]).length;
-
-    return stats;
-}
--- web/week9/3/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$host = "localhost:4306";
-	$user = "dung";
-	$pwd = "1234";
-	$sql_db = "dung";
-?>
